7612093473beb268055760dce82eaf5ffce725d7
robertfrosty <Alexander.foster.mcculloch@gmail.com>
2021-09-26 13:11:04 -0400

    Add files via upload

---
+++ ui/content.js
@@ -0,0 +1,41 @@
+function extRun() {
+	chrome.storage.sync.get('mK', function(obj) {
+		if(obj['mK'] > 0) {
+			if(window.location.host == 'www.youtube.com'){
+				hideWatched();
+				document.getElementById('contents').setAttribute('async-inject-listener', 'true');
+			}else {
+				console.log('Not Youtube');
+			}
+		} else {
+			let toremove = document.getElementsByClassName('shift-yt-deleteme');
+			for(let i=toremove.length-1;i>=0;i--){toremove[i].style.display="initial";toremove[i].style.opacity="1";toremove[i].classList.remove('shift-yt-deleteme')};
+			document.getElementById('contents').setAttribute('async-inject-listener', 'false');
+		}
+	});
+}
+
+chrome.storage.sync.get('mK', function(obj) {
+	if (obj['mK'] > 0) {
+		document.getElementById('hide_watched').checked = 'true';
+	}
+});
+
+document.getElementById('hide_watched').addEventListener('change', async() => {
+	let [tab] = await chrome.tabs.query({active:true,currentWindow:true});
+
+	chrome.storage.sync.get('mK', function(obj) {
+		if (typeof obj['mK'] == 'undefined' ) {
+			chrome.storage.sync.set({'mK':1});
+		}else if (obj['mK'] > 0) {
+			chrome.storage.sync.set({'mK':0});
+		}else if (obj['mK'] == 0){
+			chrome.storage.sync.set({'mK':1});
+		}
+	});
+
+	chrome.scripting.executeScript({
+		target:{tabId:tab.id},
+		function: extRun,
+	});
+});
